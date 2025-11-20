00d9c4a478148ad6fd03d711d07d9f3833ba8a20
unknown <lakshayjoshi15@gmail.com>
2025-11-20 17:55:25 +0530

    Made changes to app.py

--- app.py
+++ app.py
@@ -1,23 +1,47 @@
 import pickle
 from flask import Flask, request, render_template
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 app = Flask(__name__)
 model = pickle.load(open('pac.pkl', 'rb'))
-tfidf = pickle.load(open('tfidf_vectorizer.pkl','rb'))
+tfidf = TfidfVectorizer(stop_words='english')
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+    return render_template('home.html')
 
-
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    if request.method == 'POST':
-    	news = request.form['news']
-    	data = [news]
-    	vect = tfidf.transform(data)
-    	my_prediction = model.predict(vect)
-    	return render_template('prediction.html', prediction=my_prediction)
+    news = request.form['news']
+    data = [news]
+
+    vect = tfidf.fit_transform(data)
+    credible_words = [
+        "government", "official", "research", "report", "study",
+        "minister", "university", "scientists", "press conference",
+        "election commission", "department", "international agency",
+        "world health organization", "WHO", "UN"
+    ]
+    
+    adjusted_confidence = 0
+
+    for word in credible_words:
+        if word.lower() in news.lower():
+            adjusted_confidence += 0.04 
+
+    try:
+        prediction = model.predict(vect)[0]
+    except:
+        prediction = "FAKE"
+
+    if adjusted_confidence > 0.5:
+        prediction = "REAL"
+        
+    prediction = str(prediction).upper()
+
+    return render_template('prediction.html', prediction=prediction)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
